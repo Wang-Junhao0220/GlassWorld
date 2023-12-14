9293d6046a34c7a3ca41fe6af4d8d91dda746c1d
Junhax <1018080220@qq.com>
2023-12-14 19:16:47 +0800

    Trying getting bedrock with bedrock.json in survival mode

--- src/client/java/com/junhax/glassworld/Item.java
+++
@@ -1,4 +0,0 @@
-package com.junhax.glassworld;
-
-public class Item {
-}
---
+++ src/client/java/com/junhax/glassworld/iron_sand.java
@@ -0,0 +1,9 @@
+package com.junhax.glassworld;
+
+import net.minecraft.item.Item;
+
+public class iron_sand extends Item {
+    public iron_sand(Settings settings) {
+        super(settings);
+    }
+}
