60c3c6cab4b24bd965889051f1c9d9f28c7fc77b
Junhax <1018080220@qq.com>
2023-12-29 21:20:23 +0800

    Try China Glass

--- src/main/java/com/junhax/glassworld/ModBlocks.java
+++ src/main/java/com/junhax/glassworld/ModBlocks.java
@@ -1,39 +1,44 @@
 package com.junhax.glassworld;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
-import static net.minecraft.registry.RegistryKeys.ITEM_GROUP;
-
 public class ModBlocks {
     public static <T extends Block> T register(T block, String name, boolean shouldRegisterItem) {
         Identifier id = new Identifier("glassworld", name);
 
-        if(shouldRegisterItem) {
-            BlockItem blockItem = new BlockItem(block, new Item.Settings());
+        if (shouldRegisterItem) {
+            BlockItem blockItem = new BlockItem(block, new FabricItemSettings());
             Registry.register(Registries.ITEM, id, blockItem);
         }
 
         return Registry.register(Registries.BLOCK, id, block);
     }
+
     public static final Block CONDENSED_DIRT = register(
             new Block(
                     AbstractBlock.Settings.create().sounds(BlockSoundGroup.GRASS)
             ), "condensed_dirt", true);
     public static final Block SUPER_GLASS = register(
             new Block(
-                    AbstractBlock.Settings.create().sounds(BlockSoundGroup.GLASS)
+                    AbstractBlock.Settings.create().strength(100F,1145140114514F).sounds(BlockSoundGroup.GLASS).nonOpaque()
             ), "super_glass", true);
+
+//    public static final Block SUPER_GLASS  = new Block(FabricBlockSettings.create().strength(50F,100F));
+
+
+
     public static void initialize() {
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COLORED_BLOCKS).register((itemGroup) -> itemGroup.add(ModBlocks.CONDENSED_DIRT.asItem()));
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register((itemGroup) -> itemGroup.add(ModBlocks.CONDENSED_DIRT.asItem()));
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register((itemGroup) -> itemGroup.add(ModBlocks.SUPER_GLASS.asItem()));
     }
 
--- src/main/java/com/junhax/glassworld/ModItems.java
+++ src/main/java/com/junhax/glassworld/ModItems.java
@@ -1,14 +1,23 @@
 package com.junhax.glassworld;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
+import static com.junhax.glassworld.ModBlocks.SUPER_GLASS;
+
 public class ModItems {
+
+    public static final Item IRON_SAND = register(new Item(new FabricItemSettings()), "iron_sand");
+
     // We can use generics to make it, so we don't need to
     // cast to an item when using this method.
     public static <T extends Item> T register(T item, String ID) {
@@ -21,15 +30,18 @@ public class ModItems {
         // Return the registered item!
         return registeredItem;
     }
-    public static final Item POOP = register(
-            new Item(new FabricItemSettings()),
-            "poop"
-    );
+    private static final ItemGroup ITEM_GROUP = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(IRON_SAND))
+            .displayName(Text.translatable("itemGroup.glassword.super_glass"))
+            .entries((context, entries) -> {
+                entries.add(IRON_SAND);
+                entries.add(SUPER_GLASS);
+            })
+            .build();
     public static void initialize() {
+        Registry.register(Registries.ITEM_GROUP, new Identifier("glassworld", "super_glass"), ITEM_GROUP);
         ItemGroupEvents
-                // Register a "modify" event for the Ingredients item group.
                 .modifyEntriesEvent(ItemGroups.INGREDIENTS)
-                // Add the item to the group when you get access to it.
-                .register((itemGroup) -> itemGroup.add(ModItems.POOP));
+                .register((itemGroup) -> itemGroup.add(ModItems.IRON_SAND));
     }
 }
