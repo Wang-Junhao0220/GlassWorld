78066c4d40753db1a71a1dcfab414b4ff2565e42
Junhax <1018080220@qq.com>
2024-01-24 16:09:13 +0800

    Now upgrades the Enchanted Books(Sharpness)

--- src/main/java/com/junhax/glassworld/mixin/EnchantmentMixin.java
+++ src/main/java/com/junhax/glassworld/mixin/EnchantmentMixin.java
@@ -1,6 +1,5 @@
 package com.junhax.glassworld.mixin;
 
-import net.minecraft.enchantment.DamageEnchantment;
 import net.minecraft.enchantment.Enchantment;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
